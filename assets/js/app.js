b087a2d57073542a8e4c22bb82ef51aa9cf50f34
Chris Armendarez <chris.michael.armo@gmail.com>
2018-09-04 17:23:23 -0700

    Add giphy image fetch & create with button click to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ const topics = [
 // Topic buttons column -->
 const topicButtonsColumn = $("#topic-buttons"),
 // Giphy images row -->
-giphyImagesRow = $("#giphy-images"),
+giphyImagesDiv = $("#giphy-images"),
 // Topic add input -->
 topicInput = $("#topic"),
 // Topic add button -->
@@ -31,6 +31,12 @@ topicAddButton = $("#add-topic");
 
 // Functions
 // -------------------------------------------------->
+
+// Capitalize string -->
+const capitalize = str => `${str[0].toUpperCase()}${str.substring(1).toLowerCase()}`;
+
+// Title case string -->
+const title = str => str.split(" ").map(capitalize).join(" ");
 
 // Create single topic button -->
 const createSingleTopicButton = topic => (
@@ -88,6 +94,66 @@ const createTopicAppendButton = e => {
     }
 };
 
+// Create giphy image -->
+const createSingleGiphyImage = giphy => {
+    // Return image column with giphy image information plugged in -->
+    return $(`
+        <div class="m-3 col-md-4 col-lg-3 img-column">
+            <div class="card">
+                <div class="card-body text-center">
+                    <p class="card-text"><strong>Rating:</strong> ${giphy.rating.toUpperCase()}</p>
+                </div>
+                <img 
+                    class="card-img-bottom img-fluid" 
+                    src="${giphy.images.fixed_height_still.url}" 
+                    alt="${giphy.title}"
+                    data-src-still="${giphy.images.fixed_height_still.url}"
+                    data-src-reg="${giphy.images.fixed_height.url}"
+                    data-still="true" />
+            </div>
+        </div>
+    `);
+};
+
+// Create giphy images -->
+const createGiphyImages = (data, heading) => {
+    // Decode heading -->
+    const decodedHeading = title(decodeURI(heading)),
+    // Create heading row + append heading -->
+    $headingRow = $("<div class='row'>").append(`
+        <div class="col-md-12 text-center my-3">
+            <hr class="mb-0">
+            <h3 class="bg-dark text-light rounded py-2 mb-0">${decodedHeading}</h3>
+            <hr class="mt-0">
+        </div>
+    `),
+    // Create image row + append giphy image -->
+    $imageRow = $("<div class='row'>");
+    // For each giphy image, create & append to image row -->
+    data.forEach(giphy => {
+        $imageRow.append(createSingleGiphyImage(giphy));
+    });
+    // Append heading & image rows to giphy images div -->
+    giphyImagesDiv.append($headingRow, $imageRow);
+};
+
+// Get giphy images from api -->
+const getCreateGiphyImages = search => {
+    // Use search in url -->
+    const url = `http://api.giphy.com/v1/gifs/search?q=${search}&limit=10&api_key=JZkZKUtiKjvpufEfUsCKEypFx0TtbRAo`;
+    // Make ajax request with url -->
+    $.ajax({
+        url,
+        method: "GET"
+    // When response is received -->
+    }).then(res => {
+        // Get data -->
+        const {data} = res;
+        // Create & append giphy image for each data item -->
+        createGiphyImages(data, search);
+    });
+};
+
 // Main Process
 // -------------------------------------------------->
 
@@ -104,4 +170,12 @@ topicInput.on("keyup", function(e) {
         // Create + append topic button -->
         createTopicAppendButton(e);
     }
+});
+
+// When topic button is clicked -->
+topicButtonsColumn.on("click", ".btn", function(e) {
+    // Get + encode data-topic for search -->
+    const topic = encodeURI($(this).attr("data-topic"));
+    // Fetch + create giphy images with search topic -->
+    getCreateGiphyImages(topic);
 });
